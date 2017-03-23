cd6bd0520be8dbc6ce3049e0840df306c4089ef8
David Crespo <crespo.dm@gmail.com>
2017-03-23 00:58:28 -0500

    basic tweet

--- tweet.js
+++ tweet.js
@@ -1,1 +1,20 @@
-console.log('tweeting')
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_KEY,
+  consumer_secret: process.env.TWITTER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+client.post(
+  'statuses/update',
+  {
+    status: 'this is a bot tweet'
+  },
+  function(error, tweet, response) {
+    if(error) throw error;
+    console.log(tweet);  // Tweet body.
+    console.log(response);  // Raw response object.
+  }
+);
